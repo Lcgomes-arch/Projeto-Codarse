f0b5b317e5a6c25045d25b6f30aeb0799d2f5c57
Lcgomes-arch <lucianogde24@gmail.com>
2026-09-01 11:26:28 -0300

    Update Header.tsx

--- youtube-nextjs-course-plataform/app/header/Header.tsx
+++ youtube-nextjs-course-plataform/app/header/Header.tsx
@@ -1,14 +1,23 @@
+'use client';
+
+import { usePathname } from "next/navigation";
+
 export const Header = () => {
+    const currentPath = usePathname();
+
+
     return (
         <nav className="flex items-center justify-center bg-primary text-white py-4">
-            <ul className="flex gap-2 ">
-                <li>
+            <ul className="flex gap-4 items-center">
+                <li className="my-2">
                     <a href="/" className="border-2 rounded-md py-2 px-2">
                         CODARSE
                     </a>
                 </li>
                 <li>
-                    <a href="/">Página Inicial</a>
+                    <a href="/" data-active={currentPath === '/'} className= 'data-[active=true]:underline'>
+                        Página Inicial
+                    </a>
                 </li>
                 
                 <li>
