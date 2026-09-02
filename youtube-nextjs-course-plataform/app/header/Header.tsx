562c878f8792b13404118c5e118efc8ea821dd26
Lcgomes-arch <lucianogde24@gmail.com>
2026-09-02 09:48:18 -0300

    Ajuste de responsividade do header

--- youtube-nextjs-course-plataform/app/header/Header.tsx
+++ youtube-nextjs-course-plataform/app/header/Header.tsx
@@ -1,34 +1,40 @@
 'use client';
 
 import { usePathname } from "next/navigation";
+import { MdMenu } from "react-icons/md";
 
 export const Header = () => {
     const currentPath = usePathname();
 
 
     return (
-        <nav className="flex items-center justify-center bg-primary text-white py-4">
+        <nav className="flex items-center gap-6 justify-start  md:justify-center bg-primary text-white py-2 sm:py-4 px-6">
+            <button className='sm:hidden'>
+                <MdMenu size={24}></MdMenu>
+            </button>
             <ul className="flex gap-4 items-center">
                 <li className="my-2">
                     <a href="/" className="border-2 rounded-md py-2 px-2">
                         CODARSE
                     </a>
                 </li>
-                <li>
+                <li className='hidden sm:block'>
                     <a href="/" data-active={currentPath === '/'} className= 'data-[active=true]:underline'>
                         Página Inicial
                     </a>
                 </li>
                 
-                <li>
+                <li className='hidden sm:block'>
                     <a href="/cursos">Cursos</a>
                 </li>
-                <li>
+                <li className='hidden sm:block'>
                     <a href="https://codarse.com/blog" target="_blank" rel="noopener noreferrer">
                         Blog
                     </a>
                 </li>
             </ul>
+
+            <h1 className='sm:hidden'>Codarse - Página inicial</h1>
         </nav>
             
     );
