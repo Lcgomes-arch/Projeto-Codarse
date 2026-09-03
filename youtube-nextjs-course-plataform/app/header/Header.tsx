ee8565ca84fbc7bb201fda9b21bac514a6a375ae
Lcgomes-arch <lucianogde24@gmail.com>
2026-09-03 10:07:20 -0300

    Atualizações dos links, criação e ajuste de responsividade do menu

--- youtube-nextjs-course-plataform/app/header/Header.tsx
+++ youtube-nextjs-course-plataform/app/header/Header.tsx
@@ -4,6 +4,7 @@ import { usePathname } from "next/navigation";
 import { MdMenu } from "react-icons/md";
 
 export const Header = () => {
+    const [drawer, setDrawer] = useState(false);
     const [title, setTitle] = useState('CodarSe');
     const currentPath = usePathname();
 
@@ -16,10 +17,10 @@ export const Header = () => {
 
     return (
         <nav className="flex items-center gap-6 justify-start  md:justify-center bg-primary text-white py-2 sm:py-4 px-6">
-            <button className='sm:hidden'>
-                <MdMenu size={24}></MdMenu>
+            <button className='sm:hidden' onClick={() => setDrawer (true)}>
+                <MdMenu size={24} />
             </button>
-            <ul className="flex gap-4 items-center">
+            <ul className="flex gap-4 items-center" tabIndex={drawer ? -1 : undefined }>
                 <li className="my-2">
                     <a href="/" className="border-2 rounded-md py-2 px-2">
                         CODARSE
@@ -41,7 +42,30 @@ export const Header = () => {
                 </li>
             </ul>
 
-            <h1 className='sm:hidden'>Codarse - Página inicial</h1>
+                <div data-open={drawer}
+                tabIndex={drawer ? undefined : -1}
+                onClick={() => setDrawer(false)}
+                 className= 'bg-gradient-to-r from-background fixed top-0 left-0 bottom-0 right-0 transition-transform data-[open=false]:-translate-x-full' >
+                    <ul className="flex gap-4 flex-col p-4 w-60 bg-background onClick={e => e.stopPropagation()}>">
+                    
+                    <li className=''>
+                        <a href="/" data-active={currentPath === '/'} className= 'data-[active=true]:underline'>
+                            Página Inicial
+                        </a>
+                    </li>
+                    
+                    <li className=''>
+                        <a href="/cursos">Cursos</a>
+                    </li>
+                    <li className=''>
+                        <a href="https://codarse.com/blog" target="_blank" rel="noopener noreferrer">
+                            Blog
+                        </a>
+                    </li>
+                                </ul>
+                </div>
+
+            <h1 className='sm:hidden'>{title}</h1>
         </nav>
             
     );
