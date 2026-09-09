7bd677172d4f155d4f47f6bcb3625a1c1a766ec6
Lcgomes-arch <lucianogde24@gmail.com>
2026-09-09 11:28:40 -0300

    Update Section.tsx

--- youtube-nextjs-course-plataform/app/section/Section.tsx
+++ youtube-nextjs-course-plataform/app/section/Section.tsx
@@ -1,3 +1,7 @@
+'use client';
+import {useRef} from 'react';
+import {MdKeyboardArrowLeft, MdKeyboardArrowRight} from 'react-icons/md';
+
 import { Card } from "../card/card";
 
 interface ISectionProps {
@@ -6,24 +10,53 @@ interface ISectionProps {
     variant: 'grid' | 'h-list';
 }
 export const Section= ({ title, items, variant= 'grid' }: ISectionProps) => {
+
+const scrollRef = useRef<HTMLUListElement>(null);
+
+const handleScroll = (scroll: number) => {
+    const currentScrollLeft = scrollRef.current?.scrollLeft || 0;
+    scrollRef.current?.scrollTo({ behavior: 'smooth', left: currentScrollLeft + scroll });
+}
+
+
+
     return (
         <section className='flex flex-col gap-4 px-4'>
             <h2 className='font-bold text-xl'>
                 {title}
             </h2>
-            <ul 
+            <ul
+            ref = {scrollRef} 
             data-variant={variant}
             className='grid grid-cols-1 sm:grid-cols-none data-[variant=grid]:sm:grid-cols-2 data-[variant=grid]:md:grid-cols-3 data-[variant=h-list]:sm:grid-flow-col
             data-[variant=h-list]:sm:overflow-x-auto'>
-                {items.map(item =>(
-                <li key={item.title} data-variant={variant} className= 'w-full data-[variant=h-list]:sm:w-72'><Card 
-                
-          description={item.description}
-          href={item.href}
-          image={item.image}
-          title={item.title}
-          /></li>
-               ))}
+
+
+            <button
+            onClick = {() => handleScroll(-350)}
+            className= 'h-14 w-14 bg-primary rounded-full flex items-center justify-center sticky my-auto left-0 -ml-14'>
+                <MdKeyboardArrowLeft size={32} />
+            </button>
+               {items.map((item, index) => (
+    <li
+        key={`${item.href}-${index}`}
+        data-variant={variant}
+        className="w-full data-[variant=h-list]:sm:w-72"
+    >
+        <Card
+            description={item.description}
+            href={item.href}
+            image={item.image}
+            title={item.title}
+        />
+    </li>
+))}
+            <button
+            onClick = {() => handleScroll(350)}
+            className= 'h-14 w-14 bg-primary rounded-full flex items-center justify-center sticky my-auto right-0 -ml-14'>
+                <MdKeyboardArrowRight size={32} />
+            </button>
+
             </ul>
         </section>
     )
