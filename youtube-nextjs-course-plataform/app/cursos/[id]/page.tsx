8d3e61ca13ba7b5d37ce303ab8e2d7b7730b2b36
Lcgomes-arch <lucianogde24@gmail.com>
2026-09-10 10:56:22 -0300

    Create page.tsx

---
+++ youtube-nextjs-course-plataform/app/cursos/[id]/page.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+
+interface Props {
+    params: { id: string }
+}
+
+export async function generateMetadata({params}: Props): Promise<Metadata> {
+  
+return {
+    title: "CodarSe - Curso de API Rest, Node e  Typescript",
+};
+
+};
+
+export default function PageCoursesDetail({params}: Props) {
+
+
+
+    return(
+        <main className='mt-8 flex justify-center'>
+            Detalhe do curso {params.id}
+        </main>
+    )
+}
